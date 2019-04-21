a6153f5416675a293755ec4ec399a4f852843743
libyyu <389465209@qq.com>
2019-04-21 21:26:41 +0800

    Flib.h

---
+++ flib/base/IArchive.h
@@ -0,0 +1,34 @@
+#ifndef _IARCHIVE_H_
+#define _IARCHIVE_H_
+#pragma once
+#include "FType.hpp"
+////////////TODO: Will Imp
+_FStdBegin
+class IArchive
+{
+public:
+    template<typename T>
+    inline IArchive& operator<<(T v); // will generate link error
+    inline IArchive& operator<<(int8 v);
+    inline IArchive& operator<<(int16 v);
+    inline IArchive& operator<<(int32 v);
+    inline IArchive& operator<<(int64 v);
+    inline IArchive& operator<<(uint8 v);
+    inline IArchive& operator<<(uint16 v);
+    inline IArchive& operator<<(uint32 v);
+    inline IArchive& operator<<(uint64 v);
+#if FLIB_COMPILER_64BITS
+    inline IArchive& operator<<(long v);
+    inline IArchive& operator<<(ulong v);
+#endif
+    inline IArchive& operator<<(bool v);
+    inline IArchive& operator<<(float v);
+    inline IArchive& operator<<(double v);
+    inline IArchive& operator<<(const char *str);
+    inline IArchive& operator<<(char v[]);
+public:
+    virtual IArchive& Serialize();
+};
+_FStdEnd
+
+#endif//_IARCHIVE_H_
---
+++ flib/flib.h
@@ -0,0 +1,33 @@
+#ifndef _FLIB_H_
+#define _FLIB_H_
+#pragma once
+#include "base/FFunc.hpp"
+#include "base/FBuffer.hpp"
+#include "base/FString.hpp"
+#include "base/FWString.hpp"
+#include "base/FConvert.hpp"
+#include "base/FEncoding.hpp"
+#include "base/FFile.hpp"
+#include "base/FIni.hpp"
+#include "base/FLock.hpp"
+#include "base/FMD5.hpp"
+#include "base/FMemPool.hpp"
+#include "base/FAlloctor.hpp"
+#include "base/FPool.hpp"
+#include "base/FSemaphore.hpp"
+#include "base/FThread.hpp"
+#include "base/FTimer.hpp"
+#include "base/FSingleton.hpp"
+#include "base/FMemTrack.hpp"
+#include "base/FPlugin.hpp"
+#include "base/FProcess.hpp"
+#include "base/FBase64.hpp"
+#include "base/FDataTable.hpp"
+#include "base/FLogFile.hpp"
+#include "base/FConsole.hpp"
+////////////////////////////////////
+////
+
+
+
+#endif//_FLIB_H_
