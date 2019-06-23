6a302b50a62624084a860c3d415ebac34fb7b25c
libyyu <389465209@qq.com>
2019-06-23 10:32:06 +0800

    FAlloctor

--- flib/base/FAlloctor.hpp
+++ flib/base/FAlloctor.hpp
@@ -5,11 +5,11 @@
 #include "FMemory.hpp"
 
 _FStdBegin
-template<size_t T,size_t MAXSIZE,typename LOCK = FLock,size_t C = 16> 
+template<size_t T,typename LOCK = FLock,size_t C = 16> 
 class FAlloctorPool
 { 
 public: 
-	FAlloctorPool():_pBlocks(NULL),_pFreeNodes(NULL){} 
+	FAlloctorPool(): _pBlocks(NULL) , _pFreeNodes(NULL) {}
 	virtual ~FAlloctorPool()
 	{
 		_lock.lock(); 
@@ -22,20 +22,18 @@ public:
 		} 
 		_lock.unlock(); 
 	}
-
+    
+	typedef FAlloctorPool<T,LOCK,C> self_type;
+	typedef LOCK               		lock_type;
+    
 public: 
-	void*   Alloc(size_t size)
+	void*   Alloc()
 	{
-		assert(size);
-		if(0 == size)
-			return NULL;
-		if(size > T)
-			return this->_next.Alloc(size);
-
 		_lock.lock(); 
+    
 		if (_pFreeNodes == NULL) 
 		{ 
-			// PBLOCK pBlock = (PBLOCK)malloc(sizeof(BLOCK)); 
+	       // PBLOCK pBlock = (PBLOCK)malloc(sizeof(BLOCK)); 
 			PBLOCK pBlock = new BLOCK;
 			assert(pBlock);
 			if (NULL == pBlock ) 
@@ -43,7 +41,7 @@ public:
 				_lock.unlock(); 
 				return NULL; 
 			} 
-
+	        
 			pBlock->pPre = _pBlocks; 
 			pBlock->pNodes[0].pPre = NULL; 
 			for (size_t i=1;i<C;++i)  
@@ -53,36 +51,28 @@ public:
 			_pBlocks = pBlock; 
 			_pFreeNodes = &_pBlocks->pNodes[C - 1]; 
 		}
-
+	    
 		PNODE pNode = _pFreeNodes; 
 		_pFreeNodes = _pFreeNodes->pPre; 
-
+	    
 		_lock.unlock(); 
-
-		//pNode->pPre = (PNODE)T; //指向一个常指针区域
+		
+	    //pNode->pPre = (PNODE)T; //指向一个常指针区域
 		pNode->pPre = (PNODE)this;
 
 		return pNode->szBuf; 
 	}
 	void    Free(void *p)
 	{
-		size_t size = *((size_t*)p - 1); 
-		assert(size);
-		if(this != (self_type *)size)
-			return this->_next.Free(p);
-
 		PNODE pNode = (PNODE)((char*)p - sizeof(PNODE)); 
-
+    
 		_lock.lock(); 
 		pNode->pPre = _pFreeNodes; 
 		_pFreeNodes = pNode; 
 		_lock.unlock(); 
 	}
-
+    
 protected: 
-	typedef FAlloctorPool<T,MAXSIZE,LOCK,C>   self_type;
-	typedef LOCK                         lock_type;
-	typedef FAlloctorPool<2*T,MAXSIZE,LOCK,C> next_type;
 	FAlloctorPool(const self_type &Pool); 
 	self_type operator =(const self_type &Pool);    
 protected: 
@@ -91,28 +81,40 @@ protected:
 		tagNODE *       pPre; 
 		char            szBuf[T]; 
 	}NODE, *PNODE; 
-
+    
 	typedef struct tagBLOCK 
 	{
 		tagBLOCK *      pPre;
 		NODE            pNodes[C];
 	}BLOCK, *PBLOCK; 
-
+    
 protected: 
 	PBLOCK       _pBlocks; 
 	PNODE        _pFreeNodes; 
-	lock_type    _lock;
-	next_type    _next;
-}; 
+    lock_type    _lock;
+};
+
+template<typename LOCK,size_t C> 
+class FAlloctorPool<0,LOCK,C>; //MAKE ERROR
 _FStdEnd
 
 _FStdBegin
-template<size_t T,typename LOCK,size_t C> 
-class FAlloctorPool<T, T, LOCK, C>
-{ 
-public: 
-	FAlloctorPool():_pHeader(NULL){}
-	virtual ~FAlloctorPool()
+class FAlloctorPoolBase 
+{
+public:
+    FAlloctorPoolBase(){}
+    virtual ~FAlloctorPoolBase(){} 
+public:
+    virtual void* Alloc(size_t size)=0;
+    virtual void  Free(void* p)=0;
+};
+
+template <typename LOCK = FLock>
+class FAlloctorPoolMgr : public FAlloctorPoolBase
+{
+public:
+	explicit FAlloctorPoolMgr() :_pHeader(NULL) {}
+	virtual ~FAlloctorPoolMgr()
 	{
 		_lock.lock();
 		while(NULL != _pHeader)
@@ -123,97 +125,132 @@ public:
 		}
 		_lock.unlock();
 	}
-
-public: 
-	inline void* Alloc(size_t size)
+public:
+	void* Alloc(size_t size)
 	{
-		assert(size);
-
+		assert(size > 0);  
+		if (size <= 0)
+			return NULL;
+		if ( size <= 16 ) 
+			return _pool16.Alloc(); 
+		if ( size <= 32 ) 
+			return _pool32.Alloc(); 	    
+		if ( size <= 64 ) 
+			return _pool64.Alloc(); 	    
+		if ( size <= 128 ) 
+			return _pool128.Alloc(); 	    
+		if ( size <= 256 ) 
+			return _pool256.Alloc(); 	    
+		if ( size <= 512 ) 
+			return _pool512.Alloc(); 	    
+		if ( size <= 1024 ) 
+			return _pool1024.Alloc();     
+		if (size <= 2048)
+			return _pool2048.Alloc();    
+		if (size <= 4096)
+			return _pool4096.Alloc();	    
+		if (size <= 8192)
+			return _pool8192.Alloc();
+	    	    
 		size_t newsize = size + sizeof(NODE);
-		newsize =  (((newsize)+7) &~ 7);// 按8字节对齐
-
+	    newsize =  (((newsize)+7) &~ 7);// 按8字节对齐
+	    
 		PNODE pItem = (PNODE)malloc(newsize);
-
+	    
 		if(NULL == pItem)
 			return NULL;
-
+	    
 		_lock.lock();
 		pItem->pPre       = _pHeader;
-		if(NULL != _pHeader)  (_pHeader->pNext = pItem);
+	    if(NULL != _pHeader)  (_pHeader->pNext = pItem);
 		_pHeader          = pItem;
 		_lock.unlock();
 
 		pItem->size        = (size_t)this;
-
+	     
+	    //  printf("alloc pitem:%p,this:%p\n",pItem,this);
 		return (char*)pItem + sizeof(NODE);
 	}
-	inline void Free(void *p)
+	void  Free(void* p)
 	{
-		assert(p);
+		assert(NULL != p);
+		if (NULL == p ) 
+			return ; 
+	    
 		size_t size = *((size_t*)p - 1); 
 		assert(size > 0);
+	    
+	    if (this == (self_type *)size) 
+	    {    
+	        PNODE pItem = (PNODE)((char*)p - sizeof(NODE));
+	      //   printf("free  pitem:%p,this:%p\n",pItem,this);
 
-		assert(this == (self_type *)size);
-		if (this == (self_type *)size) {
-
-			PNODE pItem = (PNODE)((char*)p - sizeof(NODE));
-
-			_lock.lock();
-			if(pItem == _pHeader)
-				_pHeader = _pHeader->pPre;
-			else {
-				PNODE preNode = pItem->pPre;
-				pItem->pNext->pPre = preNode;
-				if(preNode)
-					preNode->pNext = pItem->pNext;
-			}
-			_lock.unlock();
-
-			free(pItem);
-		}
+	        _lock.lock();
+	        if(pItem == _pHeader)
+	            _pHeader = _pHeader->pPre;
+	        else 
+	        {
+	            PNODE preNode = pItem->pPre;
+	            pItem->pNext->pPre = preNode;
+	            if(preNode)
+	                preNode->pNext = pItem->pNext;
+	        }
+	        _lock.unlock();
+	        
+	        free(pItem);
+	        return;
+	    }
+	    
+	    FAlloctorPool<16,LOCK> * pool = (FAlloctorPool<16,LOCK> *)size;  
+	    return pool->Free(p);
 	}
-
+    
 protected: 
-	typedef FAlloctorPool<T,T,LOCK,C>         self_type;
-	typedef LOCK                         lock_type;
-
-	FAlloctorPool(const self_type &Pool); 
-	self_type operator =(const self_type &Pool);    
-protected: 
+	typedef FAlloctorPoolMgr<LOCK> self_type;
+	typedef LOCK    lock_type;
+	FAlloctorPoolMgr(const self_type &Pool); 
+	self_type operator =(const self_type &Pool);
+    
+protected:
 	typedef struct tagNODE {
 		tagNODE *  pPre;
 		tagNODE *  pNext;
 		size_t     size;
 	}NODE,*PNODE;
-
-protected: 
+    
+protected:
 	PNODE            _pHeader;
 	lock_type        _lock;
-}; 
-_FStdEnd
+    
+    FAlloctorPool<16,lock_type>     _pool16; 
+	FAlloctorPool<32,lock_type>     _pool32; 
+	FAlloctorPool<64,lock_type>     _pool64; 
+	FAlloctorPool<128,lock_type>    _pool128; 
+	FAlloctorPool<256,lock_type>    _pool256; 
+	FAlloctorPool<512,lock_type>    _pool512; 
+	FAlloctorPool<1024,lock_type>   _pool1024;
+	FAlloctorPool<2048,lock_type>   _pool2048;
+	FAlloctorPool<4096,lock_type>   _pool4096;
+	FAlloctorPool<8192,lock_type>   _pool8192;
+};
 
-_FStdBegin
-template<typename LOCK,size_t C> 
-class FAlloctorPool<0, 0, LOCK, C>; //MAKE ERROR
-
-
-class FMemPoolFactory
+class FAlloctorPoolFactory
 {
 public:
-    static FAlloctorPool<16,16384>* get()
+    static FAlloctorPoolMgr<FLock>* get()
     {
-        static FAlloctorPool<16,16384> __mempool;
+        static FAlloctorPoolMgr<FLock> __mempool;
         return &__mempool;
     }
 };
 
 inline void* FPoolMalloc(size_t nSize)
 {
-    return FMemPoolFactory::get()->Alloc(nSize);
+    return FAlloctorPoolFactory::get()->Alloc(nSize);
 }
 inline void  FPoolFree(void* p)
 {
-    return FMemPoolFactory::get()->Free(p);
+    return FAlloctorPoolFactory::get()->Free(p);
 }
 
 _FStdEnd
